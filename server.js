35b77b207c523a8c11c8716e6b4f93345e0aa31d
Neil <neil.tellez@gmail.com>
2020-03-02 23:10:22 -0500

    Frontend Update

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.json());
 // Enable cors
 app.use(cors());
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 app.use('/api/v1/stores', require('./routes/stores'));
 
